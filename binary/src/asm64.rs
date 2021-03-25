d57b6cb096ce216ffc32bec5df692d09bc8630df
xjd <xjd@cryptape.com>
2021-03-25 15:21:23 +0800

    asm64 with develop version of ckb-vm
    ckb-vm git hash:3bc375e3ad56d324b2521c0bf4dd1fa64f592560

--- binary/src/asm64.rs
+++ binary/src/asm64.rs
@@ -1,9 +1,14 @@
+mod cost_model;
+
 use bytes::Bytes;
-use ckb_vm::machine::asm::AsmMachine;
+use ckb_vm::machine::asm::{AsmMachine, AsmCoreMachine};
+use ckb_vm::machine::{SupportMachine, VERSION1};
+use ckb_vm::DefaultMachineBuilder;
 use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::process::exit;
+
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
@@ -14,9 +19,16 @@ fn main() {
     let buffer: Bytes = buffer.into();
     let args: Vec<Bytes> = args.into_iter().map(|a| a.into()).collect();
 
-    let mut machine = AsmMachine::default();
+    let asm_core = AsmCoreMachine::new(ckb_vm::ISA_IMC, VERSION1, 1 << 31);
+    let core = DefaultMachineBuilder::<Box<AsmCoreMachine>>::new(asm_core)
+        .instruction_cycle_func(Box::new(cost_model::instruction_cycles))
+        .build();
+    let mut machine = AsmMachine::new(core, None);
+
     machine.load_program(&buffer, &args).unwrap();
     let result = machine.run();
+    let cycles = machine.machine.cycles();
+    println!("Cycles = {:?}", cycles);
     if result != Ok(0) {
         println!("Error result: {:?}", result);
         exit(i32::from(result.unwrap_or(-1)));
